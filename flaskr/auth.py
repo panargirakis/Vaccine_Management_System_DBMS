6f93f493c1b2e80ca7d6d774a4d1c605f7e341c5
Sanika Patki <spatki@wpi.edu>
2021-05-03 21:43:27 -0400

    Schedule appt and show past  appt button implemented

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -349,10 +349,15 @@ def phase_eligibility():
 @bp.route('/show_appt', methods=('GET', 'POST'))
 def show_appt():
 
+    # Get past and upcoming appointments
     user_id = session.get('user_id')
     #print(user_id)
     qres = app.show_upcoming_appointments(user_id)
-    #qres = app.show_upcoming_appointments(741852963)
+    qres_past = app.show_past_appointments(user_id)
+    print(qres[0])
+    print(qres_past[0])
+
+    # Prompt for vaccination shots.
     try:
         if str(qres[0][8]) == 'Johnson':
             vacc_out = "No further appointments needed!"
@@ -364,17 +369,17 @@ def show_appt():
 
         vacc_out = "Please schedule your first vaccine appointment."
 
+    # Header for appointment table
     header = ("Appt Id", "Date & Time", "Location", "Street", "Apartment", "City", "State", "Country","Vaccine")
+
+    # Get past appointments: Past appointment button
+    if request.method == 'POST':
+        print("post", request.form)
+        if request.form['Show Past Appointments'] == "Show Past Appointments":
+            return render_template('auth/show_appt.html', header=header, data=qres_past)
+
     return render_template('auth/show_appt.html', header=header, data=qres, output=vacc_out)
 
-#
-# @bp.route('/schedule_appt', methods=('GET', 'POST'))
-# def schedule_appt():
-#     #qres = app.show_available_appointments("WPI")
-#     qres = app.show_available_appointments()
-#     print(qres)
-#     header = ("Date", "Location", "Phase", "Vaccine_Type","Schedule")
-#     return render_template('auth/schedule_appt.html', data=qres, header=header)
 
 @bp.route('/schedule_appt', methods=('GET', 'POST'))
 def schedule_appt():
@@ -384,32 +389,32 @@ def schedule_appt():
     cursor.execute("SELECT DISTINCT P.ssn FROM People P " \
                    "WHERE P.ssn= :ssn", [user_id])
     ssn = cursor.fetchall()
-    print("SSN:",ssn)
+    ssn_tuple = ssn[0]
+    ssn_str = str(ssn_tuple[0])
+    #print("SSN:",ssn_str)
 
-    print("request page")
+    # get avaialble appointments
     qres = app.show_available_appointments()
-    qres = [(i, ) + qres[i] for i in range(len(qres))]
-    print(qres)
+    #print(qres)
 
-    # print(qres)
-    header = ("Sr No.", "Date", "Location", "Phase", "Vaccine_Type", "Schedule")
+    header = ("Appt_Id", "Date", "Location", "Phase", "Vaccine_Type", "Schedule")
     if request.method == 'POST':
-        print("post", request.form)
+        #print("post", request.form)
         for row in qres:
             try:
                 if request.form[f'schedule{row[0]}'] == "Schedule":
-                    print("FOUND", row)
-                    # get appt id in database
-                    # insert ssn to appt table
-                    # cursor.execute(
-                    #     'INSERT INTO Address (Address_ID, Apartment, Street, City, State, Country, Zip_Code) VALUES (:Address_ID, :Apartment, :Street, :City, :State, :Country, :Zip_Code)',
-                    #     (address_id, apartment, street, city, state, country, zip_code))
-                    return render_template('auth/schedule_appt.html', data=[row], header=header)
+                    #print("FOUND", row)
+
+                    # get appt id
+                    #print(str(row[0]))
+                    cursor.execute(
+                        "UPDATE Appointments SET SSN=%s WHERE Appt_Id='%s'" % (
+                        ssn_str,str(row[0])))
+
+                    print("successfully added")
+                    break
             except Exception as e:
                 pass
-
-        # cursor = DB.get_instance()
-        # print("hello")
 
     return render_template('auth/schedule_appt.html', data=qres, header=header)
 
